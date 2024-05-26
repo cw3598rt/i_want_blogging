cc97c4dc88c862d706808e4fa49b572bc3d2e375
cw3598rt <cw3598rt@gmail.com>
2024-05-26 18:13:16 +0900

    화면 고정

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 // import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:i_want_blogging/screens/home_page.dart';
 
 Future main() async {
-  // await dotenv.load(fileName: "assets/env");
-  Gemini.init(apiKey: "AIzaSyDbiYx54k_1-G1Re3xW1hMLa14bwzeIj9A");
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+    (fnc) => runApp(
+      const ProviderScope(
+        child: MyApp(),
+      ),
     ),
   );
+  // await dotenv.load(fileName: "assets/env");
+  Gemini.init(apiKey: "AIzaSyDbiYx54k_1-G1Re3xW1hMLa14bwzeIj9A");
 }
 
 class MyApp extends StatelessWidget {
